deef0c4297050f193900ba2c456142bf95815c90
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-01-23 10:13:28 +0100

    Make it compile

--- src/savemng.cpp
+++ src/savemng.cpp
@@ -930,12 +930,6 @@ void restoreSavedata(Title *title, uint8_t slot, int8_t sdusers, int8_t allusers
     else
         srcPath = StringUtils::stringFormat("sd:/wiiu/backups/%08x%08x/%u", highID, lowID, slot);
     std::string dstPath = StringUtils::stringFormat("%s/%08x/%08x/%s", path.c_str(), highID, lowID, isWii ? "data" : "user");
-    if(!isWii) {
-        const std::string metaPath = StringUtils::stringFormat("%s/usr/save/%08x/%08x/meta",
-                                                            title->isTitleOnUSB ? getUSB().c_str() : "storage_mlc01:",
-                                                            highID, lowID);
-        createFolderUnlocked(metaPath);
-    }
     createFolderUnlocked(dstPath);
 
     if ((sdusers > -1) && !isWii) {
@@ -972,6 +966,10 @@ void restoreSavedata(Title *title, uint8_t slot, int8_t sdusers, int8_t allusers
         const std::string titleMetaPath = StringUtils::stringFormat("%s/usr/title/%08x/%08x/meta",
                                                                 title->isTitleOnUSB ? getUSB().c_str() : "storage_mlc01:",
                                                                 highID, lowID);
+        const std::string metaPath = StringUtils::stringFormat("%s/usr/save/%08x/%08x/meta",
+                                                            title->isTitleOnUSB ? getUSB().c_str() : "storage_mlc01:",
+                                                            highID, lowID);
+        createFolderUnlocked(metaPath);
         copyFile(titleMetaPath + "/meta.xml", metaPath + "/meta.xml");
         copyFile(titleMetaPath + "/iconTex.tga", metaPath + "/iconTex.tga");
     }
