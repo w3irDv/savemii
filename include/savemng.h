914e46559d03e8ec96faa91482352531731daffa
w3irDv <170813473+w3irDv@users.noreply.github.com>
2025-05-04 18:07:19 +0200

    show format of slot (hex,titlename or legacy)

--- include/savemng.h
+++ include/savemng.h
@@ -224,4 +224,5 @@ bool mkdirAndUnlink(const std::string & path);
 bool mergeTitleFolders(Title* title);
 enum STR2INT_ERROR { SUCCESS, OVERFLOW, UNDERFLOW, INCONVERTIBLE };
 STR2INT_ERROR str2int (int &i, char const *s, int base = 0);
+std::string slotFormatType(Title *title, uint8_t slot);
 
