ebd51ab99c5ea9f562c917da08a30c857e2a788e
w3irDv <170813473+w3irDv@users.noreply.github.com>
2025-07-20 10:38:11 +0200

    abort batch tasks at title boundary

--- include/savemng.h
+++ include/savemng.h
@@ -201,7 +201,7 @@ void writeMetadata(uint32_t highID,uint32_t lowID,uint8_t slot,bool isUSB,const 
 void writeMetadataWithTag(uint32_t highID,uint32_t lowID,uint8_t slot,bool isUSB,const std::string &tag) __attribute__((hot));
 void writeMetadataWithTag(uint32_t highID,uint32_t lowID,uint8_t slot,bool isUSB,const std::string &batchDatetime,const std::string &tag) __attribute__((hot));
 void writeBackupAllMetadata(const std::string & Date, const std::string & tag);
-void backupAllSave(Title *titles, int count, const std::string &batchDatetime, bool onlySelectedTitles = false) __attribute__((hot));
+int backupAllSave(Title *titles, int count, const std::string &batchDatetime, bool onlySelectedTitles = false) __attribute__((hot));
 int countTitlesToSave(Title *titles, int count, bool onlySelectedTitles = false) __attribute__((hot));
 int backupSavedata(Title *title, uint8_t slot, int8_t source_user, bool common, eAccountSource accountSource = USE_WIIU_PROFILES,const std::string &tag = "") __attribute__((hot));
 int restoreSavedata(Title *title, uint8_t slot, int8_t source_user, int8_t wiiu_user, bool common, bool interactive = true) __attribute__((hot));
@@ -223,7 +223,7 @@ uint8_t getWiiUAccn();
 Account *getWiiUAcc();
 Account *getVolAcc();
 void deleteSlot(Title *title, uint8_t slot);
-bool wipeBackupSet(const std::string &subPath);
+bool wipeBackupSet(const std::string &subPath , bool force = false);
 void splitStringWithNewLines(const std::string &input, std::string &output);
 void sdWriteDisclaimer();
 void summarizeBackupCounters(Title *titles, int titlesCount,int & titlesOK, int & titlesAborted, int & titlesWarning, int & titlesKO, int & titlesSkipped, int & titlesNotInitialized, std::vector<std::string> & failedTitles);
